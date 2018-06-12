d14cd0789defce7b718f4d0786991acb3a7b9377
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2018-06-11 23:48:16 -0300

    make simulator as package

---
+++ muxim/setup.py
@@ -0,0 +1,11 @@
+'''Setup module.'''
+from setuptools import setup, find_packages
+
+setup(
+    name='muxim',
+    version='1.0.0',
+    description='Mutual Exclusion Algorithm Simulator',
+    packages=find_packages(),
+    project_urls={},
+    install_requires='dataclasses'
+)
